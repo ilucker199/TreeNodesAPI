33fafab379ce4e391654bbd91694c2f462414f66
yuliayurakova <yyurakova@softengine.com>
2023-04-25 18:03:57 +0300

    Remove usings.

--- TreeAPI/Extensions/ExceptionHandlerExtensions.cs
+++ TreeAPI/Extensions/ExceptionHandlerExtensions.cs
@@ -1,9 +1,7 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Diagnostics;
+﻿using Microsoft.AspNetCore.Diagnostics;
 using System;
 using System.IO;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using TreeAPI.Exceptions;
--- TreeAPI/Startup.cs
+++ TreeAPI/Startup.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.IO;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Diagnostics;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using TreeAPI.Context;
-using TreeAPI.Exceptions;
 using TreeAPI.Extensions;
 
 namespace TreeAPI
